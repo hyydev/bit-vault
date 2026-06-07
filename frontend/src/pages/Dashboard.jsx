cbcf8237502af7af552c4f6df59e8fd99b83230a
Devesh sharma <devsharma13314@gmail.com>
2026-06-07 23:59:18 +0530

    completing all the ui changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,17 @@
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import {Users} from "../components/Users"
+
+
 export const Dashboard = () => {
-    return <div className="w-full h-screen flex items-center justify-center">
-        <div className="w-96 p-6 rounded-lg shadow-lg bg-white">
-            <h1 className="text-2xl font-bold mb-4">Dashboard</h1>    </div>
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"50000"} />
+            <Users />
+        </div>
+
     </div>
+
+    
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,6 +1,23 @@
-export const Signin = ()=>{
-    return <div className="w-full h-screen flex items-center justify-center">
-        <div className="w-96 p-6 rounded-lg shadow-lg bg-white">
-            <h1 className="text-2xl font-bold mb-4">Sign In</h1>    </div>
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SubHeading } from "../components/SubHeading"
+
+export const Signin = () => {
+
+    return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <Heading label={"Sign in"} />
+        <SubHeading label={"Enter your credentials to access your account"} />
+        <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
+        <InputBox placeholder="123456" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign in"} />
+        </div>
+        <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+      </div>
     </div>
+  </div>
 }
